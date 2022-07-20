8d29d368d880d7a02596932c9a43eb38b940485c
jakhongirs <j.sagdullaev31@gmail.com>
2022-07-20 14:51:49 +0500

    feat: swagger

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Posts API",
+        default_version='1.0.0',
+        description="API documentation of App",
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('news.urls')),
+    path('api/v1/swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
 ]
